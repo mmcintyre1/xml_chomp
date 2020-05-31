5671d2d81d82a6ef8671e49c200088d7cfa1a90c
Michael McIntyre <45203141+mmcintyre1@users.noreply.github.com>
2020-05-31 16:41:38 -0400

    added some tests

--- tests/test_xml_parser.py
+++ tests/test_xml_parser.py
@@ -1,16 +1,30 @@
 import pytest
 
-
 from utils import xml_parser
 
-
 ns_xml_obj = xml_parser.Xml('./test_data/namespaced_xml.xml')
+simple_xml_obj = xml_parser.Xml('./test_data/simple_xml.xml')
+xml_obj_from_string = xml_parser.Xml("""
+<?xml version="1.0"?>
+<data>
+    <country name="Liechtenstein">
+        <rank updated="yes">2</rank>
+    </country>
+    <country name="Singapore">
+        <rank updated="yes">5</rank>
+        <year>2011</year>
+    </country>
+    <country name="Panama">
+    </country>
+</data>
+""")
 
 
 @pytest.mark.parametrize(
     'test_input,expected', [
         ("/component", "/*[local-name()='component']"),
-        ("publicationMeta[@type='article']/doi", "/*[local-name()='publicationMeta'][@type='article']/*[local-name()='doi']"),
+        ("publicationMeta[@type='article']/doi",
+         "/*[local-name()='publicationMeta'][@type='article']/*[local-name()='doi']"),
         ("/component/@name", "/*[local-name()='component']/@name"),
         ("/event[@type='firstOnline']/@date", "/*[local-name()='event'][@type='firstOnline']/@date"),
         ("/component/header/contentMeta/creators/creator[@creatorRole='author']/PersonName", "")
@@ -27,6 +41,7 @@ def test_local_namer(test_input, expected):
 )
 def test_get_xpath_value(test_input, expected):
     assert ns_xml_obj.get_xpath_value(test_input, ignore_ns=True) == expected
+
 
 @pytest.mark.parametrize(
     'test_input,expected', [
@@ -47,4 +62,16 @@ def test_make_child_dict(test_input, expected):
     assert ns_xml_obj.make_child_dict(test_input, ignore_ns=True) == expected
 
 
+@pytest.mark.parametrize(
+    'test_input,expected', [
+        ({"year"}, ["/data", "/data/country", "/data/country/rank"])
+    ]
+)
+def test_get_all_xpaths_exclusions(test_input, expected):
+    # all_xpaths returns a dictionary and we only care about the xpath
+    # not the attr
+    assert list(simple_xml_obj.get_all_xpaths(exclusions=test_input).keys()) == expected
 
+
+def test_get_all_xpaths_no_exclusions():
+    assert len(simple_xml_obj.get_all_xpaths().keys()) == 7
